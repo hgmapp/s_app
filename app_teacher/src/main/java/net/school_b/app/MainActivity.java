18f368febd7e9a4e71ac7ee69a77a50ae151fbf3
hgmhwl <1510624947@qq.com>
2017-04-02 17:34:59 +0800

    first commit

---
+++ app_teacher/src/main/java/net/school_b/app/MainActivity.java
@@ -0,0 +1,15 @@
+package net.school_b.app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 测试提交
+        //  叫啊叫啊叫dasda
+        setContentView(R.layout.activity_main);
+    }
+}
